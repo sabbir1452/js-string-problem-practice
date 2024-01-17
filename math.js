bd22ab934fa91cf760d8e85a2744e43880a985fa
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-17 13:50:37 +0600

    ladj;lfkj

--- math.js
+++ math.js
@@ -1,2 +1,34 @@
 const result = Math.pow(3, 8);
-console.log(result);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+console.log(gap);
+if (gap < 5) {
+  console.log("you guys can be friends");
+} else {
+  console.log("you guys stay apart");
+}
+
+// ------------------
+//
+//-------------------
+const number = 2.5;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+const result2 = Math.ceil(2.0);
+const result3 = Math.floor(456.259);
+// console.log(result3);
+// console.log(result2);
+
+// ----------------
+// Random
+
+// console.log(Math.random());
+
+for (let i = 0; i < 20; i++) {
+  const random = Math.round(Math.random() * 6);
+  console.log(random);
+}
