63b0f7e674ec3878b9b0f343fdc473c6f0b42f64
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-17 14:39:21 +0600

    a;ldkfj

---
+++ max.js
@@ -0,0 +1,7 @@
+const jim = 69;
+const dela = 97;
+if (jim > dela) {
+  console.log("jim will git the cake");
+} else {
+  console.log("dela you will get the cake");
+}
---
+++ max3.js
@@ -0,0 +1,20 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if (jim > dela && jim > chinku) {
+  console.log("Jim will get the cake");
+} else if (dela > jim && dela > chinku) {
+  console.log("Dela will eat cake in the bela");
+} else {
+  console.log("Chinku will get the cake");
+}
+
+Math.max(12, 45);
+
+// Homework : 1
+// Write a function that will take 3 numbers will return the max number
+// Write a function that will take 3 parameters and will return teh min number
+
+// first time do it usin if-else
+// second tiem do it using Math.min or Math.max
